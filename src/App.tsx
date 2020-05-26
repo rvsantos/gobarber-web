284fbfac2c8e8c708d3e765decf51f7555942e9c
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-05-26 14:57:52 -0300

    input component and button created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Signin from './pages/Signin';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
+    <Signin />
     <GlobalStyle />
-    <h1>Hello React</h1>
   </>
 );
 
---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const Signin: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="gobarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Password"
+        />
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
+);
+
+export default Signin;
